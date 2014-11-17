54ec92b5e59d17885cd55ee2aa7e4684b24d68a2
CircularBoy <yurets097@gmail.com>
2014-11-17 19:24:16 +0200

    якоря

--- aquaclean/js/script.js
+++ aquaclean/js/script.js
@@ -69,4 +69,14 @@ $(document).ready(function() {
   });
 
 
+  $('nav a[href^="#"]').bind('click.smoothscroll', function (e) {
+    e.preventDefault();
+    var target = this.hash,
+    $target = $(target);
+    $('html, body').stop().animate({
+        'scrollTop':$target.offset().top
+    }, 900, 'swing', function () {
+        window.location.hash = target;
+    });
+});
 });
