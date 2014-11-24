44de7a4f57638a8064542a013a5f9942cf1f3b79
CircularBoy <yurets097@gmail.com>
2014-11-24 13:50:13 +0200

    1

--- aquaclean/js/script.js
+++ aquaclean/js/script.js
@@ -85,17 +85,19 @@ $(document).ready(function() {
       window.location.hash = target;
     });
   });
+
+	function initialize() {     
+		var myLatlng = new google.maps.LatLng(-34.397, 150.644);
+		var myOptions = {
+			zoom: 8,
+			center: myLatlng,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		}
+		var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions); 
+	}
+  
 });
 
-function initialize() {     
-	var myLatlng = new google.maps.LatLng(-34.397, 150.644);
-	var myOptions = {
-		zoom: 8,
-		center: myLatlng,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	}
-	var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions); 
-}
 
 $(window).load(function(){
 	totalWidth = 0
