cd616c68db8715dd9b224e8247f7e06994f02314
CircularBoy <yurets097@gmail.com>
2014-12-14 14:49:31 +0200

    132

--- aquaclean/js/script.js
+++ aquaclean/js/script.js
@@ -60,7 +60,7 @@ $(document).ready(function() {
     return false;
   });
 
-  $('header button, .block-call button, .block-7 button, .footer button').click(function() {
+  $('header button, .block-call button, .block-4 .wrapper button, .block-7 button, .footer button').click(function() {
     $('div.shadow').fadeIn(300);
   });
 
