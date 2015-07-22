ef2bd2e6e1171c6e1e2ebdafdc45b13d3e090957
Yuriy Krugoviy <yurets097@gmail.com>
2015-07-22 10:40:15 +0300

    New site profit

---
+++ profit/js/main.js
@@ -0,0 +1,83 @@
+$(document).ready(function() {
+
+  //google maps API
+  function initialize() {
+    var mapCanvas = document.getElementById('map_canvas');
+    var mapOptions = {
+      center: new google.maps.LatLng(48.41947854, 35.05973139),
+      zoom: 13, 
+      scrollwheel: false,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var markers = [],
+    myPlaces = [];
+    myPlaces.push(new Place('ProFit', 48.41947854, 35.05973139, 'Фитнесс клуб'));
+    for (var i = 0, n = myPlaces.length; i < n; i++) {
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(myPlaces[i].latitude, myPlaces[i].longitude),
+        map: map,
+        title: myPlaces[i].name
+      });
+      var infowindow = new google.maps.InfoWindow({
+       content: '<h1>' + myPlaces[i].name + '</h1><br/>' + myPlaces[i].description
+     });
+      makeInfoWindowEvent(map, infowindow, marker);
+      markers.push(marker);
+    }
+  }
+  function makeInfoWindowEvent(map, infowindow, marker) {
+    google.maps.event.addListener(marker, 'click', function() {
+     infowindow.open(map, marker);
+   });
+  }
+  function Place(name, latitude, longitude, description) {
+    this.name = name;  
+    this.latitude = latitude; 
+    this.longitude = longitude;  
+    this.description = description; 
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+  //button to top
+  $(function() {
+    $(window).scroll(function() {
+      if($(this).scrollTop() != 0) {
+        $('#arrow .arrow').fadeIn();
+      } else {
+        $('#arrow .arrow').fadeOut();
+      }
+    });
+    $('#arrow .arrow').click(function() {
+      $('body,html').animate({scrollTop:0},800);
+    });
+  });
+
+  //scroll after url click
+  $('nav a[href^="#"]').bind('click.smoothscroll', function (e) {
+    e.preventDefault();
+    var target = this.hash,
+    $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop':$target.offset().top
+    }, 900, function () {
+      window.location.hash = target;
+    });
+  });
+
+  //tabs
+  $('.tabs .titles a').click(function() {
+    $('.tabs .titles a').removeClass('active');
+    $(this).addClass('active');
+    var attr = $(this).attr('href');
+    $('.tabs .tabs-content div').fadeOut(300);
+    $(attr).fadeIn(300);
+    return false;
+  })
+
+  //Directions animation
+  $('.directions .direction').click(function() {
+    $(this).addClass('detailed');
+  })
+});
+
