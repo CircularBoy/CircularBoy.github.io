0c3e2ef46833f16dec4036554df0f77edad4db9d
Andrey Stifurak <stif.sl@mail.ru>
2015-08-02 23:06:10 +0300

    modal

--- decraft/media/js/main.js
+++ decraft/media/js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+
+  $('.modal-trigger').leanModal();
+  $(".button-collapse").sideNav();
+
   $('#nav-wrapper nav').pushpin({ top: $('nav').offset().top });
 
   $('nav ul li a').click(function() {
@@ -9,54 +13,31 @@ $(document).ready(function() {
   //button to top
   $(function() {
     $(window).scroll(function() {
-      if($(this).scrollTop() != 0) {
+      if ($(this).scrollTop() != 0) {
         $('#arrow .arrow').fadeIn();
       } else {
         $('#arrow .arrow').fadeOut();
       }
     });
+
     $('#arrow .arrow').click(function() {
-      $('body,html').animate({scrollTop:0},800);
+      $('body, html').animate({scrollTop:0},800);
     });
   });
 
-   //scroll after url click
-   $('nav a[href^="#"]').bind('click.smoothscroll', function (e) {
+  //scroll after url click
+  $('ul a[href^="#"]').bind('click.smoothscroll', function (e) {
     e.preventDefault();
     var target = this.hash,
     $target = $(target);
+
     $('html, body').stop().animate({
-      'scrollTop':$target.offset().top-40
+      scrollTop: $target.offset().top - 40
     }, 900, function () {
       window.location.hash = target;
     });
+  })
 
-    //materializee box for certificate and other
-    $('.materialboxed').materialbox();
-    
-    //ymaps
-    ymaps.ready(function(){
+  
 
-      myMap = new ymaps.Map("map", {
-        center: [44.519462, 33.580075],
-        zoom: 12
-      });
-
-      myMap.controls.add('mapTools');
-      myMap.controls.add('typeSelector');
-      myMap.controls.add('zoomControl');
-
-      var myPlacemark = new ymaps.Placemark(
-        [44.519462, 33.580075], {},
-        {
-          iconImageHref: 'images/metka.png',
-          iconImageSize: [44, 61],
-          iconImageOffset: [-22,-61]
-        }
-        );
-
-      myMap.geoObjects.add(myPlacemark);
-
-    })
-  });
- })
+});
