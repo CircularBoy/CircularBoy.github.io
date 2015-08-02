d0eacf64c6f4bd05a726a9059fbf4b9d818b75ce
Yuriy Krugoviy <yurets097@gmail.com>
2015-08-02 17:29:43 +0300

    for Stifick

--- decraft/media/js/main.js
+++ decraft/media/js/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   $('#nav-wrapper nav').pushpin({ top: $('nav').offset().top });
 
+  $('nav ul li a').click(function() {
+    $('nav ul li a').removeClass('active');
+    $(this).addClass('active');
+  });
+
   //button to top
   $(function() {
     $(window).scroll(function() {
@@ -16,20 +21,42 @@ $(document).ready(function() {
   });
 
    //scroll after url click
-  $('nav a[href^="#"]').bind('click.smoothscroll', function (e) {
+   $('nav a[href^="#"]').bind('click.smoothscroll', function (e) {
     e.preventDefault();
     var target = this.hash,
     $target = $(target);
     $('html, body').stop().animate({
-      'scrollTop':$target.offset().top-50
+      'scrollTop':$target.offset().top-40
     }, 900, function () {
       window.location.hash = target;
     });
 
     //materializee box for certificate and other
     $('.materialboxed').materialbox();
-    // $('#certificate .card').click(function() {
-    //   $(tihs).toggleClass('.box');
-    // })
+    
+    //ymaps
+    ymaps.ready(function(){
+
+      myMap = new ymaps.Map("map", {
+        center: [44.519462, 33.580075],
+        zoom: 12
+      });
+
+      myMap.controls.add('mapTools');
+      myMap.controls.add('typeSelector');
+      myMap.controls.add('zoomControl');
+
+      var myPlacemark = new ymaps.Placemark(
+        [44.519462, 33.580075], {},
+        {
+          iconImageHref: 'images/metka.png',
+          iconImageSize: [44, 61],
+          iconImageOffset: [-22,-61]
+        }
+        );
+
+      myMap.geoObjects.add(myPlacemark);
+
+    })
   });
-})
+ })
