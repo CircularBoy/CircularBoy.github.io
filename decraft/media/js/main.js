9b5a40aa9f43c56d6996697a0c4fba82b2390de1
Yuriy Krugoviy <yurets097@gmail.com>
2015-08-02 15:07:26 +0300

    decraft3

--- decraft/media/js/main.js
+++ decraft/media/js/main.js
@@ -1,5 +1,35 @@
 $(document).ready(function() {
-
   $('#nav-wrapper nav').pushpin({ top: $('nav').offset().top });
 
+  //button to top
+  $(function() {
+    $(window).scroll(function() {
+      if($(this).scrollTop() != 0) {
+        $('#arrow .arrow').fadeIn();
+      } else {
+        $('#arrow .arrow').fadeOut();
+      }
+    });
+    $('#arrow .arrow').click(function() {
+      $('body,html').animate({scrollTop:0},800);
+    });
+  });
+
+   //scroll after url click
+  $('nav a[href^="#"]').bind('click.smoothscroll', function (e) {
+    e.preventDefault();
+    var target = this.hash,
+    $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop':$target.offset().top-50
+    }, 900, function () {
+      window.location.hash = target;
+    });
+
+    //materializee box for certificate and other
+    $('.materialboxed').materialbox();
+    // $('#certificate .card').click(function() {
+    //   $(tihs).toggleClass('.box');
+    // })
+  });
 })
